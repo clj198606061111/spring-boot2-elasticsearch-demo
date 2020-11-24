90ff557aa3b1eb9c09403fb955abd8e64bb249bf
clj198606061111@163.com <clj-1986>
2020-11-24 23:16:19 +0800

    init commit

---
+++ src/main/java/com/itclj/ItcljApplication.java
@@ -0,0 +1,11 @@
+package com.itclj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItcljApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ItcljApplication.class,args);
+    }
+}
