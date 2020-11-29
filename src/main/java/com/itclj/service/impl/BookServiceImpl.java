680a956f3fd8b375788a52e430a5f543695d139a
clj198606061111@163.com <clj-1986>
2020-11-29 09:50:43 +0800

    ES分页查询

--- src/main/java/com/itclj/service/impl/BookServiceImpl.java
+++ src/main/java/com/itclj/service/impl/BookServiceImpl.java
@@ -4,6 +4,7 @@ import com.itclj.models.Book;
 import com.itclj.repositories.BookRepository;
 import com.itclj.service.BookService;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
@@ -40,52 +41,15 @@ public class BookServiceImpl implements BookService {
     }
 
     public Iterable<Book> findAll() {
-        return bookRepository.findAll();
+        Pageable pageable = PageRequest.of(1, 10);
+        return bookRepository.findAll(pageable);
     }
 
     @Override
     public Page<Book> queryPage(Integer pageNum, Integer pageSize, Book book) {
-        Pageable pageable = new Pageable() {
-            @Override
-            public int getPageNumber() {
-                return pageNum;
-            }
 
-            @Override
-            public int getPageSize() {
-                return pageSize;
-            }
-
-            @Override
-            public long getOffset() {
-                return 0;
-            }
-
-            @Override
-            public Sort getSort() {
-                return null;
-            }
-
-            @Override
-            public Pageable next() {
-                return null;
-            }
-
-            @Override
-            public Pageable previousOrFirst() {
-                return null;
-            }
-
-            @Override
-            public Pageable first() {
-                return null;
-            }
-
-            @Override
-            public boolean hasPrevious() {
-                return false;
-            }
-        };
-        return bookRepository.searchSimilar(book, null, pageable);
+        Pageable pageable = PageRequest.of(pageNum, pageSize);
+        String[] fields = {"name"};
+        return bookRepository.searchSimilar(book, fields, pageable);
     }
 }
